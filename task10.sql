c030bd3e73c7a02ae2d3bf36f8ee7b62964fbec7
abdullah107189 <abdullah107189@gmail.com>
2025-06-06 18:30:41 +0600

    extra problem solved

--- task10.sql
+++ task10.sql
@@ -53,19 +53,19 @@ INSERT INTO students ("name", age, score, department_id) VALUES
 -- Insert sample data into course_enrollments
 INSERT INTO course_enrollments (student_id, course_title, enrolled_on) VALUES
 (1, 'Introduction to Programming', '2023-01-15 09:00:00'),
-(2, 'Circuit Analysis', '2023-02-10 10:30:00'),
-(3, 'Data Structures', '2023-01-20 14:00:00'),
-(4, 'Thermodynamics', '2023-03-05 11:00:00'),
+(1, 'Circuit Analysis', '2023-02-10 10:30:00'),
+(5, 'Data Structures', '2023-01-20 14:00:00'),
+(2, 'Thermodynamics', '2023-03-05 11:00:00'),
 (5, 'Statics and Dynamics', '2023-02-28 15:30:00'),
-(6, 'Linear Algebra', '2023-01-18 08:45:00'),
-(7, 'Algorithms', '2023-03-01 09:15:00'),
+(8, 'Linear Algebra', '2023-01-18 08:45:00'),
+(6, 'Algorithms', '2023-03-01 09:15:00'),
 (8, 'Digital Logic Design', '2023-02-12 13:00:00'),
-(9, 'Quantum Mechanics', '2023-01-25 10:00:00'),
+(4, 'Quantum Mechanics', '2023-01-25 10:00:00'),
 (10, 'Machine Design', '2023-02-27 16:00:00'),
-(11, 'Organic Chemistry', '2023-03-02 12:00:00'),
-(12, 'Structural Analysis', '2023-01-22 14:30:00'),
-(13, 'Probability and Statistics', '2023-03-03 09:45:00'),
-(14, 'Genetics', '2023-02-18 11:15:00'),
+(10, 'Organic Chemistry', '2023-03-02 12:00:00'),
+(10, 'Structural Analysis', '2023-01-22 14:30:00'),
+(12, 'Probability and Statistics', '2023-03-03 09:45:00'),
+(12, 'Genetics', '2023-02-18 11:15:00'),
 (15, 'Microeconomics', '2023-01-30 10:30:00');
 
 SELECT * FROM students;
@@ -80,3 +80,52 @@ SELECT * FROM students
 -- Find students whose age is greater than the average age of all students.
 SELECT * FROM students
     WHERE age > (SELECT avg(age) from students);
+
+-- Get names of students who are enrolled in any course (use IN with subquery).
+SELECT * FROM students
+    WHERE "id" in (SELECT student_id FROM course_enrollments);
+
+-- প্রতিটি স্টুডেন্ট কয়টি কোর্সে এনরোল করেছে এবং তারা কোন কোন কোর্সে এনরোল করেছে?
+-- 1.........
+SELECT *
+FROM students s
+JOIN course_enrollments ce ON s.id = ce.student_id;
+
+-- 2................
+SELECT 
+    s.name,
+    ce.course_title
+FROM 
+    students s
+JOIN 
+    course_enrollments ce ON s.id = ce.student_id;
+
+-- 3................
+SELECT 
+    s.name,
+    COUNT(ce.course_title) AS total_courses
+FROM 
+    students s
+JOIN 
+    course_enrollments ce ON s.id = ce.student_id
+GROUP BY 
+    s.name;
+
+-- 4................
+SELECT 
+    s.name,
+    STRING_AGG(ce.course_title, ', ') AS enrolled_courses
+FROM 
+    students s
+JOIN 
+    course_enrollments ce ON s.id = ce.student_id
+GROUP BY 
+    s.name;
+
+
+-- finally
+SELECT s.name, string_agg(ce.course_title, ', ') 
+FROM students as s
+JOIN course_enrollments as ce ON s.id = ce.student_id
+GROUP BY s.name;
+
